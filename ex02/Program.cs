9e72f325873a1fca7141c13998f9f2afff66131f
Natalya <h.h.smolnikova@mail.ru>
2022-07-02 00:57:03 +0500

    задача4.1

---
+++ ex02/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает три числа и выдает мах из этих чисел.
+int a = 2;
+int b = 3;
+int c = 7;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine(max);
