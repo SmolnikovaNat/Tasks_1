81e6f4b357ad3635f9bcc1ae7ae87b2a55882c12
Natalya <h.h.smolnikova@mail.ru>
2022-07-05 23:06:35 +0500

    задача 8

---
+++ ex05/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число N, а на выходе показывает все четные числа от 1 до N
+
+Console.WriteLine("Введите первое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    if (i % 2 == 0)
+    Console.WriteLine(i);
+}
+
